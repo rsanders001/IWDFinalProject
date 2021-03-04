eee4688c75ad36820c48f1a00c3134421bfe3f14
Ryan <rsanders2k@gmail.com>
2021-03-04 10:06:04 -0600

    STEP 1

---
+++ packages/my-first-theme/src/components/index.js
@@ -0,0 +1,12 @@
+import { connect } from "frontity"
+
+const Root = ({ state }) => {
+    return (
+        <>
+        <h1>Frontity Workshop</h1>
+    <p>Current URL: {state.router.link}</p>
+    </>
+)
+}
+
+export default connect(Root)
